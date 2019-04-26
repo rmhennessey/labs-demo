6bed4bea1eb6cc2ea0ecc6a5d7532778a6d73692
RMH <rory.hennessey@gmail.com>
2019-04-25 21:35:21 -0400

    added index.html page for the static landing page

--- twitter-blocker/src/App.js
+++ twitter-blocker/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+
+// import MuiButton from '@material-ui/core/Button';
+// import { BUTTON } from '../../theme/core';
+
+// const Button = ({ className, large, inverted, danger, ...props }) => (
+//   <MuiButton
+//     className={cx(
+//       BUTTON.root,
+//       className,
+//       inverted && BUTTON.inverted,
+//       danger && BUTTON.danger,
+//       large && BUTTON.large,
+//     )}
+//     {...props}
+//   />
+// );
+
+// export default Button;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button a href="/auth/twitter">Sign in with Twitter</Button>
     </div>
   );
 }
